e601c98b1929ffd29f222398c48c3cb1b17be8a0
korex71 <kore.dv7@gmail.com>
2021-06-01 18:15:27 -0300

    Search functionally

--- src/contexts/AppContext.tsx
+++ src/contexts/AppContext.tsx
@@ -7,27 +7,62 @@ import React, {
   useContext,
 } from "react";
 
+import api from "../api/config";
 interface IContextData {
-  hello: string;
-  hi: string;
-  setHi: Dispatch<SetStateAction<string>>;
+  inputSearch: string;
+  setInputSearch: Dispatch<SetStateAction<string>>;
+  searchResults: ISearchResults[];
+  handleSearch: () => void;
 }
 
+interface ISearchResults {
+  title: string;
+  type: string;
+  thumbnails: Array<Object>;
+  bestThumbnail?: {
+    url: string;
+    width: number;
+    height: number;
+  };
+  id: string;
+  author: {
+    avatars: Array<Object>;
+    bestAvatar?: { url: string };
+    name: string;
+    channelID: string;
+  };
+  isLive?: boolean;
+  duration?: string;
+  uploadedAt?: string;
+  views: number;
+}
 interface IProps {
   children: ReactNode;
 }
 
-const AppContext = createContext({} as IContextData);
+export const AppContext = createContext({} as IContextData);
 
 export default function ContextProvider({ children }: IProps) {
-  const [hi, setHi] = useState("");
+  const [inputSearch, setInputSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  function handleSearch() {
+    if (!inputSearch) return;
+    api
+      .get(`/search/${inputSearch}`)
+      .then((res) => res.data)
+      .then((data) => setSearchResults(data))
+      .catch((err) => console.warn(err));
+    console.log(inputSearch);
+  }
 
   return (
     <AppContext.Provider
       value={{
-        hello: "world",
-        hi,
-        setHi,
+        inputSearch,
+        setInputSearch,
+        searchResults,
+        handleSearch,
       }}
     >
       {children}
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
-
+import React, { useContext } from "react";
+import SearchSongItem from "../component/SearchSongItem";
+import { AppContext } from "../contexts/AppContext";
 export default function Search() {
+  const { searchResults } = useContext(AppContext);
+
   return (
-    <div id="search">
+    <div>
       <div className="row">
         <h3 className="text-center search-text text-white">Pesquisa</h3>
       </div>
       <div className="container-fluid mt-3 mb-5 list-videos">
-        <h1>Ainda nada aqui...</h1>
+        {searchResults.map((item, key) => {
+          return <SearchSongItem key={key} title={item.title} />;
+        })}
       </div>
     </div>
   );
