12c0665839572873d0e72e384abcf83ab0c53aaf
korex71 <kore.dv7@gmail.com>
2021-06-01 11:59:07 -0300

    Routes

---
+++ src/pages/Library.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Library() {
+  return (
+    <div id="library" className="library-list text-white">
+      <h1>Library</h1>
+    </div>
+  );
+}
---
+++ src/pages/Search.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Search() {
+  return (
+    <div id="search">
+      <div className="row">
+        <h3 className="text-center search-text text-white">Pesquisa</h3>
+      </div>
+      <div className="container-fluid mt-3 mb-5 list-videos">
+        <h1>Ainda nada aqui...</h1>
+      </div>
+    </div>
+  );
+}
