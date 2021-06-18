f54ff974898de5d7da74cac7aeb2fe5748ad6af1
korex71 <kore.dv7@gmail.com>
2021-06-18 11:46:22 -0300

    Unauthenticated routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import AppContext from "./contexts/AppContext";
 import App from "./App";
+import Login from "./component/Login";
 
 ReactDOM.render(
-  <BrowserRouter basename="/">
-    <AppContext>
-      <App />
-    </AppContext>
+  <BrowserRouter>
+        <AppContext>
+            <Switch>
+              <Route path="/" component={App} exact />
+              <Route path="/login" component={() => <Login/>} />
+
+            </Switch>
+        </AppContext>
   </BrowserRouter>,
   document.getElementById("root")
 );
